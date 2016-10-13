c4a24f384c82c65c68fbe410951a01e07d933831
Gabriel Estes <gabriel.estes@gmail.com>
2016-10-12 20:13:26 -0400

    complete phone numbers

--- lib/separator.rb
+++ lib/separator.rb
@@ -8,5 +8,12 @@ class Separator
     end
   end
 
-  
+  def phone_number(number)
+    result = {}
+    match = number.match(/\(?(\d{3})\)?[- \.]?(\d{3})[- \.]?(\d{4})/)
+      return nil unless match
+    result[:area_code] = match[1].to_s
+    result[:number] = match[2].to_s + '-' + match[3].to_s
+    result
+  end
 end
