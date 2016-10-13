c5a85986d6048c2d771f63f109f97e32c02bb81e
Gabriel Estes <gabriel.estes@gmail.com>
2016-10-12 22:44:34 -0400

    tests pass through line 43

--- lib/separator.rb
+++ lib/separator.rb
@@ -10,10 +10,19 @@ class Separator
 
   def phone_number(number)
     result = {}
-    match = number.match(/\(?(\d{3})\)?[- \.]?(\d{3})[- \.]?(\d{4})/)
-      return nil unless match
-    result[:area_code] = match[1].to_s
-    result[:number] = match[2].to_s + '-' + match[3].to_s
+    phone = number.match(/\(?(\d{3})\)?[- \.]?(\d{3})[- \.]?(\d{4})/)
+      return nil unless phone
+    result[:area_code] = phone[1].to_s
+    result[:number] = phone[2].to_s + '-' + phone[3].to_s
+    result
+  end
+
+  def money(cash)
+    result = {}
+    result[:currency] = (cash.match(/\$/)).to_s
+    reduced = cash.match(/([^$\s]\d*[\.|\S|\,]?\d*?[\.|\S|\,]?\d*[\.|\S|\,]?\d*[\.|\S|\,]?\d*[\.|\S|\,]?)/)
+    reduced = reduced[0].gsub(/[\,]/, '')
+    result[:amount] = reduced.to_f.round(2)
     result
   end
 end
